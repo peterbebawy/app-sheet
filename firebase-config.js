a92d616a747494d1ed52aa4029e26f78707e7fed
peterbebawy <115377883+peterbebawy@users.noreply.github.com>
2026-08-13 20:54:55 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,11 @@
-/* ===================== FIREBASE CONFIG ===================== */
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDO2or6mbwfE_YW-V-u4JAEuHIOLgDzaNw",
+  apiKey: "YOUR_API_KEY",
   authDomain: "app-sheet-18928.firebaseapp.com",
+  databaseURL: "https://app-sheet-18928-default-rtdb.firebaseio.com",
   projectId: "app-sheet-18928",
   storageBucket: "app-sheet-18928.firebasestorage.app",
   messagingSenderId: "1024207589778",
-  appId: "1:1024207589778:web:f41fa9fb58dfe135ddf14b",
-
-  // ضع رابط Realtime Database الحقيقي هنا
-  databaseURL: "https://app-sheet-18928-default-rtdb.firebaseio.com"
+  appId: "1:1024207589778:web:f41fa9fb58dfe135ddf14b"
 };
 
 if (!firebase.apps.length) {
